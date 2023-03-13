8aec39b5e86ac5f357fff26efbf5793ee51889eb
kulig <kuli.gabor@telekom.hu>
2023-03-13 17:20:18 +0100

    add main

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './component/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'',
+    component:MainComponent,
+    data : {title:'Main', side: false},
+    children : []},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
