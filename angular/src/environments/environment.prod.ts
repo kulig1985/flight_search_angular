303c18b964930db5a9e716e3d7641b64a3220c8c
kulig <kuli.gabor@telekom.hu>
2023-03-15 15:17:31 +0100

    env mod

--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   all_flight_request : "http://95.138.193.102:2233/api/getAllFlightSearchRequest",
-  all_flight_result : "http://95.138.193.102:2233/api/getAllFlightSearchInstanceByFsreId?fsreId=",
+  all_flight_result_instance : "http://95.138.193.102:2233/api/getAllFlightSearchInstanceByFsreId?fsreId=",
   all_flight_result_details : "http://95.138.193.102:2233/api/getFlightResultByFsinIn?fsinId="
 };
